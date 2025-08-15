994e91e6cce8c7c1f8b0982944c7df798ef379ab
Bruno Torres <brunotorres.sl024@academico.ifsul.edu.br>
2025-08-15 20:27:14 -0300

    aula dia 15/08/2025

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-    runApp(aplicativo());
+  runApp(aplicativo());
 }
 
 class aplicativo extends StatelessWidget {
-   aplicativo({super.key});
+  aplicativo({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,13 +14,51 @@ class aplicativo extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           centerTitle: true,
-          title: Text("Meu app"),
+
+          title: Text(
+            "Meu app",
+            style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+          ),
           backgroundColor: Colors.red,
           foregroundColor: Colors.white,
         ),
-        body: Text("Conteudo atual do app"),
+
+        body: Center(
+          child: Container(
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text("1.1", style: TextStyle(fontSize: 20),),
+                      Text("1.2", style: TextStyle(fontSize: 20),),
+                      Text("1.3", style: TextStyle(fontSize: 20))
+                    ],
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    "Conteudo atual do app",
+                    style: TextStyle(fontSize: 20.00, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Isso aqui é um botao",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
 }
-
